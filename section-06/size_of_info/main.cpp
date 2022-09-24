14c25aaf7561b58c18555efa34e394eacf6506a8
amit-curiosity <amit.curiosity@gmail.com>
2022-09-24 20:14:21 +0530

    size_of_information

---
+++ section-06/size_of_info/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+int32_t main(int32_t argc, char *argv[]) {
+    std::cout << "sizeof information" << std::endl;
+
+    std::cout << "***************************************" << std::endl;
+    std::cout << "char: " << sizeof(char) << " bytes." << std::endl;
+    std::cout << "signed int : " << sizeof(int) << " bytes." << std::endl;
+    std::cout << "unsigned int: " << sizeof(unsigned int) << " bytes." << std::endl;
+    std::cout << "signed short: " << sizeof(short) << " bytes." << std::endl;
+    std::cout << "signed long: " << sizeof(long) << " bytes." << std::endl;
+    std::cout << "signed long long: " << sizeof(long long) << " bytes." << std::endl;
+
+    std::cout << "***************************************" << std::endl;
+    std::cout << "float: " << sizeof(float) << " bytes." << std::endl;
+    std::cout << "double: " << sizeof(double) << " bytes." << std::endl;
+    std::cout << "long double: " << sizeof(long double) << " bytes." << std::endl;
+
+    std::cout << "***************************************" << std::endl;
+    std::cout << "Minimum values:" << std::endl;
+    std::cout << "char: " << CHAR_MIN << std::endl;
+    std::cout << "signed int: " << INT_MIN << std::endl;
+    std::cout << "signed short: " << SHRT_MIN << std::endl;
+    std::cout << "signed long: " << LONG_MIN << std::endl;
+    std::cout << "signed long long: " << LLONG_MIN << std::endl;
+
+    std::cout << "***************************************" << std::endl;
+    std::cout << "Maximum values:" << std::endl;
+    std::cout << "char: " << CHAR_MAX << std::endl;
+    std::cout << "signed int: " << INT_MAX << std::endl;
+    std::cout << "signed short: " << SHRT_MAX << std::endl;
+    std::cout << "signed long: " << LONG_MAX << std::endl;
+    std::cout << "signed long long: " << LLONG_MAX << std::endl;
+    return 0;
+}
